3bf3669af730d6a66d0f17ec4bf818b9818a77d9
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-09-24 12:22:29 +0530

    upto Settings

--- app/src/main/java/com/example/midgetpixels/Home.java
+++ app/src/main/java/com/example/midgetpixels/Home.java
@@ -2,7 +2,11 @@ package com.example.midgetpixels;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Button shopbutton= findViewById(R.id.shopBtn);
+
+        shopbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, Shopping.class);
+                startActivity(intent);
+            }
+        });
+   Button chatbutton= findViewById(R.id.chatBtn);
+
+        chatbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, Chat.class);
+                startActivity(intent);
+            }
+        });
+
+        Button notificationBtn= findViewById(R.id.notificationBtn);
+        notificationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, AppNotification.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
